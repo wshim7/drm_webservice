3f9aadf10d78af26ec76cdb52bceff7e6a052167
wshim7 <wshim7@gatech.edu>
2019-02-14 16:46:44 -0500

    google oauth2 test

--- src/main/java/edu/drm/app/controllers/appController.java
+++ src/main/java/edu/drm/app/controllers/appController.java
@@ -1,8 +1,15 @@
 package edu.drm.app.controllers;
 
+import java.security.Principal;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.drm.app.dao.UserRepository;
@@ -26,4 +33,16 @@ public class appController {
 		return userRepository.save(dto.toEntity());
 	}
 	
+	@GetMapping(value = "/")
+	public String index() {
+		OAuth2User user = getCurrentUser();
+		return "Hello " + user.getAttributes().get("name") + ". Your email is " + user.getAttributes().get("email")
+				  + " and your profile picture is <img src='"+user.getAttributes().get("picture")+"' /> <br />"
+				  + "<a href='/logout'>logout</a>";
+	}
+	
+	public OAuth2User getCurrentUser() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return ((OAuth2AuthenticationToken)auth).getPrincipal();
+	}
 }
