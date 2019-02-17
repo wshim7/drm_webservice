270eb19fe94d15b95411145bf374ee005acae9ea
wshim7 <wshim7@gatech.edu>
2019-02-17 00:46:09 -0500

    revert back

--- src/main/java/edu/drm/app/controllers/appController.java
+++ src/main/java/edu/drm/app/controllers/appController.java
@@ -1,15 +1,9 @@
 package edu.drm.app.controllers;
 
-import java.security.Principal;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.drm.app.dao.UserRepository;
